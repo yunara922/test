b1dd97b7d37bb7081e947e670fcbccfe9cc20e64
tinolwmf <tinolwm.f@docomo.ne.jp>
2018-02-05 21:34:31 +0900

    180205

--- script.js
+++ script.js
@@ -1,10 +1,6 @@
-var test1=80;
-var test2=96;
-
-if(test1>=80){
-    if(test2<=89){
-        document.write("不合格");
-    }else if(test2>=90){
-        document.write("合格");
-    }
+function totalPrice(price, tax=1.08,haiso=400){
+    return(price+haiso)*tax;
 }
+
+document.write(totalPrice(300)+"<br>");
+document.write(totalPrice(450)+"<br>");
