4ccfe2f4062acc3d5c5061e823ca3de29eab45eb
yunara922 <funk.244@gmail.com>
2018-02-14 21:40:27 +0900

    180214

--- script.js
+++ script.js
@@ -1,6 +1,12 @@
-function totalPrice(price, tax=1.08,haiso=400){
-    return(price+haiso)*tax;
-}
-
-document.write(totalPrice(300)+"<br>");
-document.write(totalPrice(450)+"<br>");
+$(function(){
+    $("p").hover(
+        function(){
+            $("img").fadeIn();
+        },
+    
+    function(){
+        $("img").fadeOut();    
+    });   
+    
+    
+});
